416b068e4fb2130edb6836d1ed2e383a4bb2e22a
parkksss <yesleee9211@gmail.com>
2022-03-19 11:28:24 +0900

    creat simpe code _week1

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let name = "박예슬"
+  const styles = {
+    border: '1px solid #eee',
+    padding: '20px',
+    width: '200px',
+    margin: '30px auto'
+  };
 
   return (
     <div className="App">
-      {name}
-      <p style={{color: 'orange', fontSize: '20px'}}>안녕하세요! 리액트 반입니다 :)</p>
-      <input type="text" />
+      <div style={styles}>
+        <h1 style={{color: 'green'}}>안녕하세요!</h1>
+        <hr />
+        <p style={{textAlign: 'left'}}>이름을 입력해주세요.</p>
+        <input type="text" />
+      </div>
     </div>
   );
 }
