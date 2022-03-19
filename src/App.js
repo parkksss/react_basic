3e2953c26e9e00579e041b31c0aa5cf9ac1c8ac4
parkksss <yesleee9211@gmail.com>
2022-03-19 17:16:43 +0900

    practice Functional Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import LifecycleEx from './LifecycleEx';
+// BucketList 컴포넌트를 import 해옵니다.
+// import [컴포넌트 명] from [컴포넌트가 있는 파일경로];
+import BucketList from './BucketList';
 
 function App() {
+
   return (
     <div className="App">
-      <LifecycleEx/>
+      <h1>내 버킷리스트</h1>
+      {/* 컴포넌트를 넣어줍니다. */}
+      <BucketList/>
     </div>
   );
 }
 
-export default App;
+export default App; 
