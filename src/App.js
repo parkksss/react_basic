e90fd0816178d653426d38f1de876d777d297da1
parkksss <yesleee9211@gmail.com>
2022-03-19 16:52:07 +0900

    practice LifecycleEx

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import LifecycleEx from './LifecycleEx';
 
 function App() {
-  const styles = {
-    border: '1px solid #eee',
-    padding: '20px',
-    width: '200px',
-    margin: '30px auto'
-  };
-
   return (
     <div className="App">
-      <div style={styles}>
-        <h1 style={{color: 'green'}}>안녕하세요!</h1>
-        <hr />
-        <p style={{textAlign: 'left'}}>이름을 입력해주세요.</p>
-        <input type="text" />
-      </div>
+      <LifecycleEx/>
     </div>
   );
 }
