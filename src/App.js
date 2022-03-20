bc6afb76557cfc360aad61d2d5c6d4cc4555f0bf
parkksss <yesleee9211@gmail.com>
2022-03-20 18:38:53 +0900

    give styled-components to bucketList

--- src/App.js
+++ src/App.js
@@ -1,51 +1,54 @@
-import React from 'react';
+import React from "react";
+import logo from "./logo.svg";
 // BucketList 컴포넌트를 import 해옵니다.
 // import [컴포넌트 명] from [컴포넌트가 있는 파일경로];
-import BucketList from './BucketList';
-import "./style.css";
+import BucketList from "./BucketList";
 import styled from "styled-components";
 
+// 클래스형 컴포넌트는 이렇게 생겼습니다!
 class App extends React.Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     // App 컴포넌트의 state를 정의해줍니다.
     this.state = {
-      list: ['영화관 가기', '매일 책읽기', '수영 배우기'],
+      list: ["영화관 가기", "매일 책읽기", "수영 배우기"],
     };
   }
 
   // 랜더 함수 안에 리액트 엘리먼트를 넣어줍니다!
   render() {
-    // this 키워드를 통해 state에 접근할 수 있어요.
-    console.log(this.state);
-
-      return (
+    return (
       <div className="App">
-        <MyStyled>
-          <p>im here!!!</p>
-        </MyStyled>
-        {/* <div className="container">
-          <h1>내 버킷리스트</h1>
-          <hr className="line"/>
-          <BucketList list={this.state.list}/>
-        </div> */}
+        <Container>
+          <Title>내 버킷리스트</Title>
+          <Line/>
+          {/* 컴포넌트를 넣어줍니다. */}
+          {/* <컴포넌트 명 [props 명]={넘겨줄 것(리스트, 문자열, 숫자, ...)}/> */}
+          <BucketList list={this.state.list} />
+        </Container>
       </div>
     );
   }
 }
 
+const Container = styled.div`
+  max-width: 350px;
+  min-height: 80vh;
+  background-color: #fff;
+  padding: 16px;
+  margin: 20px auto;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+`;
 
-const MyStyled = styled.div`
-  width: 50vw;
-  height: 150px;
-  background-color: ${(props) => (props.bg_color ? "red" : "purple")};
-  p {
-    color: blue;
-  }
-  &:hover{
-    background-color: yellow;
-  }
+const Title = styled.h1`
+  color: slateblue;
+  text-align: center;
+`;
+
+const Line = styled.hr`
+  margin: 16px 0px;
+  border: 1px dotted #ddd;
 `;
 
 export default App;
--- src/BucketList.js
+++ src/BucketList.js
@@ -1,5 +1,6 @@
 // 리액트 패키지를 불러옵니다.
-import React from 'react'; 
+import React from 'react';
+import styled from "styled-components";
 
 // 함수형 컴포넌트는 이렇게 쓸 수도 있고
 // function Bucketlist(props){
@@ -17,24 +18,31 @@ const BucketList = ({ list }) => {
     const my_lists = list;
     // 컴포넌트가 뿌려줄 ui 요소(리엑트 엘리먼트라고 불러요.)를 반환해줍니다.
     return (
-        <div>
-            {
-                // js의 내장 함수 중 하나인 map입니다. 리스트의 갯수만큼 => 오른쪽 구문을 반복해요. 
-                // 자세한 사용법은 아래 링크를 확인해주세요.
-                // https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/map
-                my_lists.map((list, index) => {
-                    // 콘솔을 확인해봅시다 :)
-                    console.log(list);
-                    return (
-                        <div className="list-item" key={index}>
-                          {list}
-                        </div>
-                      );
-                })
-            }
-        </div>
+        <ListStyle>
+          {my_lists.map((list, index) => {
+            return (
+              <ItemStyle key={index}>
+                {list}
+              </ItemStyle>
+            );
+          })}
+        </ListStyle>
     );
-}
+};
+
+const ListStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: auto;
+`;
+
+const ItemStyle = styled.div`
+  padding: 16px;
+  margin: 8px;
+  background-color: aliceblue;
+`;
 
 // 우리가 만든 함수형 컴포넌트를 export 해줍니다.
 // export 해주면 다른 컴포넌트에서 BucketList 컴포넌트를 불러다 쓸 수 있어요.
