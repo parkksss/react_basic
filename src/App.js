361e61223e6abe95cffcad8048564d333f6ecf4f
parkksss <yesleee9211@gmail.com>
2022-03-21 01:36:34 +0900

    add function to add items to bucket list

--- src/App.js
+++ src/App.js
@@ -1,59 +1,65 @@
 import React from "react";
-import logo from "./logo.svg";
-// BucketList 컴포넌트를 import 해옵니다.
-// import [컴포넌트 명] from [컴포넌트가 있는 파일경로];
 import BucketList from "./BucketList";
 import styled from "styled-components";
 
-// 클래스형 컴포넌트는 이렇게 생겼습니다!
 class App extends React.Component {
   constructor(props) {
     super(props);
-    // App 컴포넌트의 state를 정의해줍니다.
+
     this.state = {
       list: ["영화관 가기", "매일 책읽기", "수영 배우기"],
     };
-    // ref는 이렇게 선언합니다! 
+
     this.text = React.createRef();
   }
 
-  componentDidMount(){
-		// 콘솔에서 확인해보자!
-    console.log(this.text);
+  componentDidMount() {  
   }
 
+  addBucket = () => {
+    console.log(this.text.current.value);
+    const new_item = this.text.current.value;
+    // ... => 스프레드 문법
+    // [...this.state.list, 넣고 싶었던 어떤 값]
+    this.setState({ list: [...this.state.list, new_item] });
+  }
 
-  // 랜더 함수 안에 리액트 엘리먼트를 넣어줍니다!
   render() {
+    
     return (
-      <div className="App">
+      <AppWrap className="App">
         <Container>
           <Title>내 버킷리스트</Title>
-          <Line/>
-          {/* 컴포넌트를 넣어줍니다. */}
-          {/* <컴포넌트 명 [props 명]={넘겨줄 것(리스트, 문자열, 숫자, ...)}/> */}
+          <Line />
           <BucketList list={this.state.list} />
         </Container>
 
-        <div>
-          <input type="text" ref={this.text}
-          onChange={() => {
-            console.log(this.text.current.value);
-          }}/>
-        </div>
-      </div>
+        <InputWrap>
+          <input type="text" ref={this.text} />
+          <button onClick={this.addBucket}>추가하기</button>
+        </InputWrap>
+      </AppWrap>
     );
   }
 }
 
+const AppWrap = styled.div`
+  background-color: #eee;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+`;
+
 const Container = styled.div`
-  max-width: 350px;
-  min-height: 80vh;
   background-color: #fff;
+  width: 50vw;
+  max-width: 350px;
+  margin: auto;
+  height: 80vh;
   padding: 16px;
-  margin: 20px auto;
-  border-radius: 5px;
   border: 1px solid #ddd;
+  border-radius: 5px;
 `;
 
 const Title = styled.h1`
@@ -63,7 +69,17 @@ const Title = styled.h1`
 
 const Line = styled.hr`
   margin: 16px 0px;
-  border: 1px dotted #ddd;
+`;
+
+
+const InputWrap = styled.div`
+  background-color: #fff;
+  width: 50vw;
+  max-width: 350px;
+  margin: auto;
+  padding: 16px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
 `;
 
 export default App;
