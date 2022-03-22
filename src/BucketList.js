1c6d5ec95cc6d32098b6381b97c0ce9acdb5db0b
parkksss <yesleee9211@gmail.com>
2022-03-22 16:07:06 +0900

    get redux data to BucketList

--- src/BucketList.js
+++ src/BucketList.js
@@ -1,13 +1,12 @@
-// 리액트 패키지를 불러옵니다.
 import React from "react";
 import styled from "styled-components";
 
 import { useHistory } from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
 
 const BucketList = (props) => {
   const history = useHistory();
-  console.log(props);
-  const my_lists = props.list;
+  const my_lists = useSelector((state) => state.bucket.list);
   
   return (
     <ListStyle>
